18392ca080231ca9bf88d196c6ef61ce3b6b49c6
wangmeng <wangmeng255@gmail.com>
2016-05-24 14:57:38 -0700

    finish v1.0

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
 	var key = Math.floor((Math.random() * 100) + 1);
 	var count = 0;
+  var pre = [];
 	console.log(key);
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -18,27 +19,41 @@ $(document).ready(function(){
   		newGame();
   	});
 
-  	$("#guessButton").submit(function() {
+  	$("form").submit(function( event ) {
+      event.preventDefault();
   		var guess = parseInt($("#userGuess").val());
-  		console.log(guess);
+      $("#userGuess").val("");
+  		console.log("submit "+guess);
   		if(!guess || (guess<1 && guess>100)) alert("Please input an integer between 1 and 100!");
   		else {
-  			if(guess > key) $("h2").text("Warm");
-  			else {
-  				if(guess < key) $("h2").text("Cold");
-  				 else $("h2").text("Just Right!");
-  				count += 1;
-  				$("#count").text(String(count));
-  			}
+        if(pre.indexOf(guess) === -1) {
+          pre.push(guess);
+          var temp = Math.abs(guess - key);
+    			if(temp>=50) $("#feedback").text("Ice cold");
+    			if(temp>=30 && temp<50) $("#feedback").text("Cold");
+    			if(temp>=20 && temp<30) $("#feedback").text("Warm");
+          if(temp>=10 && temp<20) $("#feedback").text("Hot");
+          if(temp>=1 && temp<10) $("#feedback").text("Very hot");
+          if(temp === 0) $("#feedback").text("Just right!");
+    			count += 1;
+    			$("#count").text(String(count));
+          $("#guessList").append("<li>"+String(guess)+"</li>");
+        }
+        else {
+          alert(guess + " is already guessed");
+        }
   		}
   	});
+
   	function newGame() {
   		key = Math.floor(Math.random() * 100) + 1;
   		count = 0;
   		console.log(key);
+      pre.length = 0;
   		$("#count").text("0");
   		$("#userGuess").val("");
   		$("#feedback").text("Make your Guess!");
+      $("#guessList li").remove();
   	}
 });
 
